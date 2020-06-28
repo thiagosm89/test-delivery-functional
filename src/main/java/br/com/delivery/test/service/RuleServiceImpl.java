a4cffaa513e2c4137c020ac7892ccbd608e810de
Thiago Pereira Mello <thiago_pmello@terceiros.sicredi.com.br>
2020-06-28 17:42:24 -0300

    Ajustado para manter as regras do predicate na base de dados

--- src/main/java/br/com/delivery/test/service/RuleServiceImpl.java
+++ src/main/java/br/com/delivery/test/service/RuleServiceImpl.java
@@ -2,17 +2,37 @@ package br.com.delivery.test.service;
 
 import br.com.delivery.test.model.Bill;
 import br.com.delivery.test.model.Rule;
+import io.vavr.Function1;
 import org.springframework.stereotype.Service;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-import static br.com.delivery.test.function.RuleSortFn.*;
+import java.util.function.Predicate;
 
 @Service
 public class RuleServiceImpl implements RuleService {
+    private static final ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
     private final List<Rule> rulesPipeline;
+
+    //Pode vir de banco de dados
+    private final static String LTE_THREE = "function(x) x > 0 && x <= 3";
+    private final static String LTE_FIVE = "function(x) x > 0 && x <= 5";
+    private final static String GT_FIVE = "function(x) x > 5";
+
+    //Define o predicate de forma dinâmica, permitindo que esteja salvo em banco a lógica funcional
+    private final static Function1<String, Predicate<Long>> RULE_PREDICATE_FN = (stringFn) -> {
+        try {
+            String predicateName = Predicate.class.getName();
+            return (Predicate<Long>) engine.eval(String.format("new %s(%s)", predicateName, stringFn));
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+        return null;
+    };
 
     public RuleServiceImpl() {
         rulesPipeline = new ArrayList<>();
@@ -21,9 +41,9 @@ public class RuleServiceImpl implements RuleService {
 
     private void definePipeline() {
         //A ordem será levada em consideração
-        rulesPipeline.add(RULE_01);
-        rulesPipeline.add(RULE_02);
-        rulesPipeline.add(RULE_03);
+        rulesPipeline.add(Rule.of(2, 0.1, RULE_PREDICATE_FN.apply(LTE_THREE)));
+        rulesPipeline.add(Rule.of(3, 0.2, RULE_PREDICATE_FN.apply(LTE_FIVE)));
+        rulesPipeline.add(Rule.of(5, 0.3, RULE_PREDICATE_FN.apply(GT_FIVE)));
     }
 
     @Override
